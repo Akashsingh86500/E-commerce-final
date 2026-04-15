4e7e19ca6438e94f6a5514903b61202e056ba843
Akash Singh <akchaudhary86500@gmail.com>
2026-04-15 23:06:46 +0530

    add client URL

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin:  ["http://localhost:5173", "http://localhost:5174"],
+    origin:  ["http://localhost:5173", "http://localhost:5174", "https://e-commerce-final-beta.vercel.app"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
